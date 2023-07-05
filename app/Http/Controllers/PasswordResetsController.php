c6c8be28290e983ceb7ea6d3129e4fbfdc8a9bcf
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2023-07-05 10:47:57 -0300

    Creación de Tablas, Controllers y DER_v1

---
+++ app/Http/Controllers/PasswordResetsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Password_Resets;
+use Illuminate\Http\Request;
+
+class PasswordResetsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Password_Resets  $password_Resets
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Password_Resets $password_Resets)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Password_Resets  $password_Resets
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Password_Resets $password_Resets)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Password_Resets  $password_Resets
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Password_Resets $password_Resets)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Password_Resets  $password_Resets
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Password_Resets $password_Resets)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_07_05_132537_create_auditoria_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auditoria', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('fecha_hora');
+            $table->integer('usuario_id')->unsigned();
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+            $table->string('accion');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auditoria');
+    }
+};
