1de7d8606cbc8dd95d4b5d7b9aebc8b270720a8c
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2023-07-05 20:12:33 -0300

    Creación de formulario de Login y algunas rutas

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,24 +13,7 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     
-    public function register(Request $request){
-        //VALIDACIÓN DE DATOS
-
-        //CREACIÓN DE NUEVO USUARIO
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-
-        $user->save();
-
-        //AUTENTICACIÓN
-        Auth::login($user);
-
-        return redirect(route('inicio'));
-
-
-    }
+  
 
     public function login(Request $request){
         
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuariosController;
 use App\Http\Controllers\AuditoriaController;
-
 use App\Http\Controllers\LoginController;
 
 
-Route::get('/', function () {
-    return view('login');
-});
+
+/*************RUTAS PARA LOGIN**************/
+
+Route::view('/login', "login")->name('login');
+Route::view('/registro', "agregar")->name('registro'); //RECICLO AGREGAR
+Route::view('/privada', "secret")->name('privada');
+
+Route::post('/validar-registro', [LoginController::class, 'register'])->name('validar-registro');
+Route::post('/inicia-sesion', [LoginController::class, 'login'])->name('inicia-sesion');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+/*******************************************/
 
 //AUDITORIA
 Route::get('/auditoria', [AuditoriaController::class, 'index']) -> name('auditoria.index');
